fe66a770306db08b40e081e2862328fd46f77414
Debra Gordon <debra.m.gordon@gmail.com>
2016-08-01 17:55:35 -0500

    started laying out the exercise

---
+++ alpha.js
@@ -0,0 +1,3 @@
+// Create a stackLetters function that should console.log rows of letters. It should take an array containing the letters of the alphabet and output them like this:
+
+// Then add logic to stackLetters that places a space after every fifth letter, like this:
