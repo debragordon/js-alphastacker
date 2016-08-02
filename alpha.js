1d6252d1f934f6abc77eef689937fe3b7a21a682
Debra Gordon <debra.m.gordon@gmail.com>
2016-08-01 19:09:47 -0500

    finished exercise,need to merge to master

--- alpha.js
+++ alpha.js
@@ -1,3 +1,23 @@
-// Create a stackLetters function that should console.log rows of letters. It should take an array containing the letters of the alphabet and output them like this:
+// Create an alphabet array to reference in the for loop.
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Then add logic to stackLetters that places a space after every fifth letter, like this:
+// Create variable that will be the result of the loop written to the console.
+var alphaString = "";
+
+// decide to use a for loop to save time ... jump into the array and write the desired element to the console
+for (var i = 0; i < alphabet.length; i++) {
+
+// adding to var alphabet with +=
+  alphaString += alphabet[i];
+
+// nest an if statement to test for the 5th letter, determined that if it is divisible by 5 it will be the 5th element and will get a space after it
+  if ((i + 1) % 5 === 0) {
+
+// answer (alphaString) now gets added (every 5th time)
+    alphaString += " ";
+  }
+
+// loop will console out the alphabet until the .length funciton is satisfied
+  console.log(alphaString);
+
+}
